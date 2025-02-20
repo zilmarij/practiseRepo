f0c4f7d132f3d2f63ec686549fcbdcb77c8d747e
zilmarij <zilmarij1229@gmail.com>
2025-02-20 20:52:10 +0530

    add: hunk

--- sample.py
+++ sample.py
@@ -1,7 +1,10 @@
 def func1():
     print("This is function 1")
+    return
 def fun2():
     print("This is function 2")
 if __name__ == "__main__":
     func1()
     fun2()
+    print("Terminating program..."+"\n")
+    print("Byeee!")
