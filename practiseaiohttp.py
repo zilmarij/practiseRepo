747e62ef61494968007b54034f1e6446ce6175d2
zilmarij <zilmarij1229@gmail.com>
2025-02-28 22:06:34 +0530

    aiohttp: started

--- practiseaiohttp.py
+++ practiseaiohttp.py
@@ -1,2 +1,25 @@
 import aiohttp
-import aiodns
+# import aiodns
+import asyncio
+
+import selectors
+
+class MyPolicy(asyncio.DefaultEventLoopPolicy):
+   def new_event_loop(self):
+      selector = selectors.SelectSelector()
+      return asyncio.SelectorEventLoop(selector)
+
+async def main():
+
+    async with aiohttp.ClientSession() as session:
+        async with session.get('http://python.org') as response:
+
+            print("Status:", response.status)
+            print("Content-type:", response.headers['content-type'])
+
+            html = await response.text()
+            print("Body:", html[:15], "...")
+
+asyncio.set_event_loop_policy(MyPolicy())
+asyncio.run(main())
+
