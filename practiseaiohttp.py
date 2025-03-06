365906f2f29311e2833d1400403f826263d63260
zilmarij <zilmarij1229@gmail.com>
2025-03-06 17:24:10 +0530

    wip: aiohttp with bs

--- practiseaiohttp.py
+++ practiseaiohttp.py
@@ -1,9 +1,8 @@
 import aiohttp
 # import aiodns
 import asyncio
-
 import selectors
-
+from bs4 import BeautifulSoup as bs
 class MyPolicy(asyncio.DefaultEventLoopPolicy):
    def new_event_loop(self):
       selector = selectors.SelectSelector()
@@ -14,11 +13,16 @@ async def main():
     async with aiohttp.ClientSession() as session:
         async with session.get('http://python.org') as response:
 
-            print("Status:", response.status)
-            print("Content-type:", response.headers['content-type'])
+            # print("Status:", response.status)
+            # print("Content-type:", response.headers['content-type'])
 
             html = await response.text()
-            print("Body:", html[:15], "...")
+            soup = bs(html,'html.parser')
+            
+            # print(html)
+            # print("Body:", html[:15], "...")
+            
+            print(soup.find(class_='skip-link'))
 
 asyncio.set_event_loop_policy(MyPolicy())
 asyncio.run(main())
